5c83bb4c38371a7058cb525acd025bc0a636de65
kendrickpc <kennethpchang@gmail.com>
2019-10-13 22:19:01 +0800

    Second problem in exercise solved.

---
+++ 9_strings/exercises/9.22.2.py
@@ -0,0 +1,23 @@
+'''
+In Robert McCloskey’s book Make Way for Ducklings, the names of the ducklings
+are Jack, Kack, Lack, Mack, Nack, Ouack, Pack, and Quack. This loop tries to
+output these names in order.
+'''
+
+prefixes = "JKLMNOPQ"
+suffix = "ack"
+
+
+for p in prefixes:
+    if p == "O" or p == "Q":
+        print(p + "u" + suffix)
+    else:
+        print(p + suffix)
+
+# Of course, that’s not quite right because Ouack and Quack are misspelled.
+# Can you fix it?
+
+# Fixed.
+
+
+
