348b3bf76cebb3ce2da7aed170a0153120b014d8
kendrickpc <kennethpchang@gmail.com>
2019-10-13 16:38:48 +0800

    Chapter 9 exercises started.

---
+++ 9_strings/exercises/9.22.1.py
@@ -0,0 +1,64 @@
+# What is the result of each of the following:
+
+'''
+1. ‘Python’[1]
+
+2. “Strings are sequences of characters.”[5]
+
+3. len(“wonderful”)
+
+4. ‘Mystery’[:4]
+
+5. ‘p’ in ‘Pineapple’
+
+6. ‘apple’ in ‘Pineapple’
+
+7. ‘pear’ not in ‘Pineapple’
+
+8. ‘apple’ > ‘pineapple’
+
+9. ‘pineapple’ < ‘Peach’
+'''
+
+'''
+My Answers: 
+
+#1 <<< y
+#2 <<< g
+#3 <<< 9
+#4 <<< Myst
+#5 <<< True
+#6 <<< True
+#7 <<< True
+#8 <<< False
+#9 <<< False
+'''
+
+one = 'Python'[1]
+print(one)
+
+two = 'Strings are sequences of characters.'[5]
+print(two)
+
+three = len("wonderful")
+print(three)
+
+four = 'Mystery'[:4]
+print(four)
+
+five = 'p' in 'Pineapple'
+print(five)
+
+six = 'apple' in 'Pineapple'
+print(six)
+
+seven = 'pear' not in 'Pineapple'
+print(seven)
+
+eight = 'apple' > 'pineapple'
+print(eight)
+
+nine = 'pineapple' < 'Peach'
+print(nine)
+
+
