ccc772f00ce0bf3520c1f1df2f7b05bb09009461
kendrickpc <kennethpchang@gmail.com>
2019-10-21 14:01:55 +0800

    Chapter 11 files section started.

---
+++ 11_files/11.1.working_data_files.py
@@ -0,0 +1,11 @@
+# 11.1 Working with Data Files
+'''
+Method Name     use                     Explanation
+
+open            open(filename,'r')      Open a file called filename and use it for reading. This will return a reference to a file object.
+
+open            open(filename,'w')      Open a file called filename and use it for writing. This will also return a reference to a file object.
+
+close           filevariable.close()    File use is complete.
+
+'''
---
+++ 11_files/11.3.reading_a_file.py
@@ -0,0 +1,24 @@
+# 11.3 Reading a File
+'''
+As an example, suppose we have a text file called qbdata.txt that contains 
+the following data representing statistics about NFL quarterbacks. Although
+it would be possible to consider entering this data by hand each time it
+is used, you can imagine that it would be time-consuming and error-prone
+to do this. In addition, it is likely that there could be data from more
+quarterbacks and other years. The format of the data file is as follows:
+
+First Name, Last Name, Position, Team, Completions, Attempts, Yards, TDs, Ints, Comp%, Rating
+
+To open this file, we would call the open function. The variable,
+fileref, now holds a reference to the file object returned by open.
+When we are finished with the file, we can close it by using the close
+method. After the file is closed any further attempts to use fileref will
+result in an error.
+
+    >>>fileref = open("qbdata.txt", "r")
+    >>>
+    >>>fileref.close()
+    >>>
+
+'''
+
